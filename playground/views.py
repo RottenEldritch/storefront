c8aaf2d5ca941c4c637e5c2772177b513c44e5c7
RottenEldritch <anvithvittal@outlook.com>
2022-09-19 22:07:39 +0530

    Created a working view that generates an html document with a variable when reaching the page Wrapped python syntaxes in html Created additional apps namely store and tags and assigned them in the project settings.py

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+# receives request and returns a response
+# request handler
+# also called an action
+
+def calculate():
+    x = 1
+    y = 2
+    return x
+def say_hello(request):
+    x = calculate()
+    return render(request, 'hello.html', {'name': 'RottenEldritch '})
--- storefront/settings.py
+++ storefront/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     ## 'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playground'
+    'playground',
+    'store',
+    'tags',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +51,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 ROOT_URLCONF = 'storefront.urls'
